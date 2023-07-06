fe373e034fe3f8d275991f08356ec189ab550235
temeculous <zacerious@gmail.com>
2023-07-06 18:58:59 -0400

    New show route, new Model, model registered with admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    type = models.CharField(max_length=50)
+    color = models.CharField(max_length=40)
+    info = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.type
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
-finches = [
-    {'type': 'House finch', 'color': 'Rosey', 'info': 'The house finch is a bird in the finch family Fringillidae. It is native to western North America and has been introduced to the eastern half of the continent and Hawaii.'},
-    {'type': 'Purple Finch', 'color': 'Purple','info': 'This species and the other "American rosefinches" were formerly included with the rosefinches of Eurasia in the genus Carpodacus; however, the three North American species are not closely related to the rosefinches of the Old World, and have thus been moved to the genus Haemorhous by most taxonomic authorities.'},
-    {'type': 'Crossbill', 'color': 'Red','info': 'Crossbills are birds of the genus Loxia within the finch family, with six species. These birds are characterized by the mandibles with crossed tips, which gives the group its English name.'},
-]
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -14,4 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
